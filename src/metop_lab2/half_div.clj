eef9eee561604fadec18532c498125cb836fa56f
Даниил <deadxraver@yandex.ru>
2025-03-20 22:54:37 +0300

    доделал метод половинного деления + main

--- src/metop_lab2/half_div.clj
+++ src/metop_lab2/half_div.clj
@@ -1,16 +1,25 @@
 (ns metop-lab2.half-div)
 
+(def str-format "|\t%d\t|\t%.3f\t|\t%.3f\t|\t%.3f\t|\t%.3f\t|\t%.3f\t|\t%.3f\t|\n")
+
 (def f (fn [x] (+ (* x x) (* -3 x) (* x (Math/log x)))))
 
 (def x1 (fn [a b eps] (/ (+ a b (- 0 eps)) 2)))
 (def x2 (fn [a b eps] (/ (+ a b eps) 2)))
 (def xm (fn [a b] (/ (+ a b) 2)))
 
-(def find-extremum
-  (fn [a b eps] (
-                  let [y1 (f (x1 a b eps))
-                       y2 (f (x2 a b eps))
-                       a1 (if (> y1 y2) (x1 a b eps) a)
-                       b1 (if (> y1 y2) b (x2 a b eps))]
-                  (if (> (- b1 a1) (* 2 eps)) (find-extremum a1 b1 eps) (f (xm a1 b1)))
-                  )))
+(def find-minimum
+  (fn
+    ([a b eps iter] (
+                      let [y1 (f (x1 a b eps))
+                           y2 (f (x2 a b eps))
+                           a1 (if (> y1 y2) (x1 a b eps) a)
+                           b1 (if (> y1 y2) b (x2 a b eps))]
+                      (printf str-format iter a b (x1 a b eps) (x2 a b eps) y1 y2)
+                      (if (> (- b1 a1) (* 2 eps)) (recur a1 b1 eps (+ iter 1)) [(xm a1 b1) (f (xm a1 b1))])
+                      ))
+    ([a b eps]
+     (println "|\t№\t|\ta\t\t|\tb\t\t|\tx1\t\t|\tx2\t\t|\ty1\t\t|\ty2\t\t|")
+     (find-minimum a b eps 1)
+     )
+    ))
--- src/metop_lab2/main.clj
+++ src/metop_lab2/main.clj
@@ -1,8 +1,13 @@
 (ns metop-lab2.main
-  (:require [metop-lab2.half-div :as half-div]))
+  (:require [metop-lab2.half-div :as half-div]
+            [metop-lab2.golden-ratio :as golden-ratio])
+  )
 
-(def a 1)
-(def b 2)
-(def eps 0.005)
+(def a 1.0)
+(def b 2.0)
+(def eps 0.0001)
 
-(println "Метод половинного деления:" (half-div/find-extremum a b eps))
+(println "Метод половинного деления:")
+(def h-d (half-div/find-minimum a b eps))
+(printf "x_m = %.3f; y_m = %.3f\n" (get h-d 0) (get h-d 1))
+(println "Метод золотого сечения:")
